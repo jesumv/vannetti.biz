17ed838b9dab1fbd3167f59b41631bd5fff76e05
Jesus Maldonado <jmaldonadoca@gmail.com>
2016-06-01 17:21:39 -0500

    se añade archivo portal

---
+++ portal.php
@@ -0,0 +1,36 @@
+<?php
+    phpinfo();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vanneti.biz</title>
+  <!-- Insert link to styles here -->
+   <link rel="stylesheet" type="text/css" href="css/inline.css">
+</head>
+<body>
+
+  <header class="header">
+    <h1 class="header__title">La red de Vannetti</h1>
+    
+  </header>
+
+  <main class="main">
+    <!-- Insert forecast-card.html here -->
+  <h2>  PORTAL VANNETTI CONSULENTI</h2>
+  </main>
+
+  <div class="dialog-container">
+    <!-- Insert add-new-city-dialog.html here -->
+  </div>
+
+  
+
+  <!-- Insert link to app.js here -->
+  
+</body>
+</html>
+
