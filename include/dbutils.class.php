a34aef341dd7eed7029c8484c002bc938da1c793
Jesus Maldonado <jmaldonadoca@gmail.com>
2016-08-26 21:26:07 -0500

    v3.1 oc y pedido completo con iva.probado con 1 articulo

--- include/dbutils.class.php
+++ include/dbutils.class.php
@@ -14,7 +14,33 @@
 			
 		}
 		
-	   public function conecta() {
+		
+		public function iva ($mysqli,$producto,$precio){
+			//esta funcion calcula el iva para un monto dado
+			//inicializar iva
+				$iva=0;
+			//revisar si el producto lleva iva
+			$sql2= "SELECT iva FROM productos WHERE idproductos = $producto";
+			$resulta = mysqli_query($mysqli,$sql2);
+            if($resulta){
+            	$row2 = mysqli_fetch_array($resulta);
+					$result3=$row2['iva'];
+					if($result3 == true){
+					$sql= "SELECT valornum FROM parametros WHERE idparametros = 1";
+	            	$result = mysqli_query($mysqli,$sql);
+	            	$row = mysqli_fetch_array($result);
+					$result2=$row['valornum'];
+					$iva = $result2*$precio;
+	            	/* liberar la serie de resultados */
+	                  mysqli_free_result($resulta);
+				}
+              return $iva;  
+            }
+            else {
+                 die('error en calculo iva ');	
+		}
+			
+		}public function conecta() {
 	    /***esta funcion establece la conexion a sql***/
 		/***variables de conexion ***/
 		$mysql_hostname = "localhost";
@@ -139,22 +165,6 @@
 		
 		
 		
-		
-		public function numinv($mysqli){
-			//lee el ultimo movimiento del inventario. esta funcion no ha sido aplicada, porque marco inexistente, revisar
-			$sql= "SELECT MAX(idinventarios) FROM inventarios";
-            $result = mysqli_query($mysqli,$sql);
-            $result2 = mysqli_fetch_row($result);
-			$dato = $result2[0];
-            if($result2){
-              return $dato;  
-            }
-            else {
-                 die('no hay resultados para la consulta de movimiento de inventario');
-            }
-			
-		}
-		
 		public function leeprov($mysqli,$idprov){
 			//lee el nombre corto del proveedor cuyo id se pasa como parametro
 			
@@ -184,135 +194,8 @@
 			
 		}
         	
-		public function llenaarts($mysqli,$remision,$codigo,$descripcion,$precio,$cantidad,$importe){
-			/*llena la tabla artremision con los articulos de cada remision*/
-			$table = 'artremision';
-			$sqlCommand= "INSERT INTO $table (codigo,remision,descripcion,precio_unitario,cantidad,importe)
-	    	VALUES ($codigo,$remision,'$descripcion',$precio,$cantidad,$importe)"
-	    or die('insercion cancelada '.$table);
-			
-	    // Execute the query here now
-	    if($query=mysqli_query($mysqli, $sqlCommand)){
-	    	return 0;
-	    }else{
-	    	return $mysqli->connect_error;
-	    }
-	    
-			
-		}
-		
-	
 	}/*** fin de la clase ***/
 	
-	class otrasdbutils{
-		function __construct() {
-			
-		}
-		
-		public function ultcliente($mysqli){
-			/*esta funcion trae el ultimo numero de cliente registrado */
-			$sql= "SELECT MAX(idclientes) FROM clientes";
-			$result = mysqli_query($mysqli,$sql);
-            $result2 = mysqli_fetch_row($result);
-			$dato = $result2[0];
-            if($result2){
-              return $dato;  
-			}
-			
-			 /* liberar la serie de resultados */
-	   				mysqli_free_result($result); 	
-		}
-		
-		public function ultcliente2($mysqli){
-			/*esta funcion trae el ultimo numero de cliente registrado y su nombre corto */
-			$sql= "SELECT MAX(idclientes)FROM clientes";
-			$result = mysqli_query($mysqli,$sql);
-            $result2 = mysqli_fetch_row($result);
-			$cliente = $result2[0];
-			$sql2 = "SELECT idclientes, nom_corto FROM clientes WHERE idclientes =".$cliente;
-			$result3 = mysqli_query($mysqli,$sql2);
-            $result4 = mysqli_fetch_row($result3);
-			 /* liberar la serie de resultados */
-	   		mysqli_free_result($result); 
-			return $result4; 	
-		}
-		
-		public function nomcliente($mysqli,$idclientes){
-			/*esta funcion trae el nombre corto del cliente proporcionado*/
-			$sql= "SELECT nom_corto FROM clientes where idclientes =".$idclientes;
-			$result = mysqli_query($mysqli,$sql);
-            $result2 = mysqli_fetch_row($result);
-			 /* liberar la serie de resultados */
-	   		mysqli_free_result($result); 
-			return $result2; 	
-		}
-		
-		
-		
-		public function estadorem($tipo, $estado){
-			/* esta funcion determina el estado de una remision en funcion de los parametros */
-			if ($tipo == 2) {
-				$resul = "EN BLANCO";
-			}else{
-							switch ($estado) {
-			    case 5:
-			      $resul = "CANCELADA";
-			        break;
-			    case 10:
-			        $resul = "FACTURADA";
-			        break;
-				default:
-			       	$resul = "VIGENTE";
-			        break;
-			}
-				
-			}
-			
-			return $resul;
-		}
-		
-		public function cancelarem($mysqli,$cancelada,$nueva){
-			/* esta funcion recibe un numero de remision, y cambia su estado a 5 cancelada x substitucion */
-			$rollo = "ESTA REMISION SE SUBSTITUYE X LA NO. ".$nueva;
-			$sqlCommand = "UPDATE remisiones SET status =5, obser= '$rollo' WHERE idremisiones= $cancelada LIMIT 1";
-			 // Execute the query here now
-	    	$query = mysqli_query($mysqli, $sqlCommand) or die ("error en actualizacion estado ".mysqli_error($mysqli)); 
-			if(mysqli_errno($mysqli)){
-				return mysqli_errno($mysqli);
-			}else{return 0;}		
-		}
-		
-		public function consultaremi($mysqli,$remi,$nivel,$remitido){
-			/*esta funcion consulta los articulos de una remision*/
-			/*recibe como parametros el numero de remision y el nivel del cliente*/
-			//CONSTANTES---------------------------------------------------------------------------------------------------------
-    		//arreglo para la lista de articulos
-        	$arts= array();
-			//determinacion del precio del producto
-			$preciorev= "t2.precio".$nivel;
-			$query= "SELECT  t1.codigo,t2.descripcion,".$preciorev.",t1.cantidad,t1.importe,t2.alg FROM artremision as t1 
-			left join productos as t2 on t1.codigo=t2.codigo WHERE  remision=".$remi;
-			$datoart= mysqli_query($mysqli,$query)or die ("Error en la consulta de articulos de la remision.".mysqli_error($mysqli));
-			$indice = 0;
-			while ($fila = mysqli_fetch_row($datoart)) {
-			       $arts[$indice][0]= $fila[0];
-				if ($remitido==2){
-					$arts[$indice][1] = $fila[1]." ALG:".$fila[5];
-				}else{
-					$arts[$indice][1] = $fila[1];
-				}
-			      
-			       $arts[$indice][2]= $fila[2];
-				   $arts[$indice][3] = $fila[3];
-				   $arts[$indice][4] = $fila[4];
-				   $arts[$indice][5] = $fila[5];
-			       $indice++;
-			    }
-					return $arts;	
-						
-			}
-		
-		
-	}/*** fin de la clase ***/
+
 	
 ?>
--- php/enviaped.php
+++ php/enviaped.php
@@ -11,6 +11,7 @@
     	/*** checa login***/
     	$funcbase->checalogin($mysqli);
 		
+	
 	function traepedmax($mysqli){
 		$req = "SELECT MAX(idpedidos) FROM pedidos WHERE 1"; 
     	$result = mysqli_query($mysqli,$req);
@@ -33,6 +34,7 @@
     $cte = $_POST["cte"];
 	$totarts=$_POST["totarts"];
 	$tipoventa= $_POST["tipoventa"];
+	$facturar= $_POST["facturarp"];
 	$prods=$_POST["prods"];
 	$cants=$_POST["cants"];
 	$preciou = $_POST["preciou"];
@@ -44,17 +46,20 @@
 	$iva=0;
 	//definicion de status segun tipo de compra
 	if($tipoventa ==0){$status = 99;}else{$status = 1;}
-	//creacion de pedido en tabla pedido
-	$sqlCommand= "INSERT INTO pedidos (idclientes,arts,monto,total,usu,status)
-	    	VALUES ($cte,$totarts,$total,$total,'$usu',$status)";
-	    	$query=mysqli_query($mysqli, $sqlCommand)or die("error en alta oc:".mysqli_error($mysqli)); 
+	if($facturar==true){$ivaped=$total*.16;}else{$ivaped=0;}
+	//1 creacion de pedido en tabla pedido
+	$sqlCommand= "INSERT INTO pedidos (idclientes,arts,monto,iva,total,usu,status)
+	    	VALUES ($cte,$totarts,$total,$ivaped,$total+$ivaped,'$usu',$status)";
+	    	$query=mysqli_query($mysqli, $sqlCommand)or die("error en alta pedidos:".mysqli_error($mysqli)); 
 			//obtencion de numero de orden de pedido
 			$nped = traepedmax($mysqli);
 			if($query){
+				$ivaped;
+				$iva =array();
 				//se inicia asumiendo que el resultado es ok.
 				$jsondata['success'] = 0;
 	//---------------CICLO POR CADA PRODUCTO DEL PEDIDO
-	//insercion de productos en tabla artspedidos
+	//2 insercion de productos en tabla artspedidos
 				$indi = 0;
 	//inicializacion de costo de ventas
 				$cvtas= 0;;
@@ -68,9 +73,12 @@
 							if($query){
 								//incremento del cv del pedido
 								$cvtas = $cvtas+$cvtaprod;
+								//incremento del iva de la orden
+								$ivaprod = $funcbase->iva($mysqli,$id,$preciot[$indi]);
+								$iva[]=$ivaprod;
 								//registro de movimientos segun tipo de venta
 								if($tipoventa==0){
-									//inventarios		
+									//3 inventarios	si es pedido de contado	
 									$sqlCommand= "INSERT INTO inventario (idproductos,tipomov,cant,usu,idoc,monto,haber)
 									VALUES ($id,2,-$cants[$indi],'$usu',$nped,$cvtaprod,$cvtaprod)";
 									$query=mysqli_query($mysqli, $sqlCommand)or die("error en salida invent: ".mysqli_error($mysqli));
@@ -90,29 +98,43 @@
 								$jsondata['total'] =$total;
 								//si el resultado de las anteriores operaciones es ok, se continua con los movimientos contables
 								if($jsondata['success'] == 0){
-									//caja y bancos
-									$sqlCommand= "INSERT INTO disponible (idmovto,idclientes,cuenta,usu,debe)
-									VALUES ($nped,$cte,1,'$usu',$total)";
-									$query=mysqli_query($mysqli, $sqlCommand)or die("error en abono caja: ".mysqli_error($mysqli));
-									if(!$query){
-										$jsondata['success'] = -4;	
+									//si es venta contado
+									if($tipoventa==0){
+										//4 caja y bancos
+										$sqlCommand= "INSERT INTO disponible (idmovto,idclientes,cuenta,usu,debe)
+										VALUES ($nped,$cte,1,'$usu',$total)";
+										$query=mysqli_query($mysqli, $sqlCommand)or die("error en abono caja: ".mysqli_error($mysqli));
+										if(!$query){
+											$jsondata['success'] = -4;	
+										}
+										//4a ingresos por ventas
+										$sqlCommand= "INSERT INTO ingresos (idmovto,idclientes,usu,haber)
+										VALUES ($nped,$cte,'$usu',$total)";
+										$query=mysqli_query($mysqli, $sqlCommand)or die("error en abono ingresos: ".mysqli_error($mysqli));
+										if(!$query){
+											$jsondata['success'] = -5;	
+										}
+										//4b costo de ventas
+										$sqlCommand= "INSERT INTO cvtas (idmovto,idclientes,usu,debe)
+										VALUES ($nped,$cte,'$usu',$cvtas)";
+										$query=mysqli_query($mysqli, $sqlCommand)or die("error en abono cvtas: ".mysqli_error($mysqli));
+										if(!$query){
+											$jsondata['success'] = -5;	
+										}
+									//si es a credito, solo se registra el pedido.
 									}	
-									//ingresos por ventas
-									$sqlCommand= "INSERT INTO ingresos (idmovto,idclientes,usu,haber)
-									VALUES ($nped,$cte,'$usu',$total)";
-									$query=mysqli_query($mysqli, $sqlCommand)or die("error en abono ingresos: ".mysqli_error($mysqli));
-									if(!$query){
-										$jsondata['success'] = -5;	
-									}
-									//costo de ventas
-									$sqlCommand= "INSERT INTO cvtas (idmovto,idclientes,usu,debe)
-									VALUES ($nped,$cte,'$usu',$cvtas)";
-									$query=mysqli_query($mysqli, $sqlCommand)or die("error en abono cvtas: ".mysqli_error($mysqli));
-									if(!$query){
-										$jsondata['success'] = -5;	
-									}
 									
-									//iva por pagar
+									//si es contado, y facturable, se registra el iva
+									//6 iva por pagar
+									
+									if($facturar== 'true'){$ivacalc= array_sum($iva);}else{$ivacalc=0;}
+									
+									$sqlCommand= "INSERT INTO ivatrans (idmovto,idclientes,usu,status,haber)
+										VALUES ($nped,$cte,'$usu',0,$ivacalc)";
+										$query=mysqli_query($mysqli, $sqlCommand)or die("error en abono ivatrans: ".mysqli_error($mysqli));
+										if(!$query){
+											$jsondata['success'] = -6;	
+										}
 								}
 								
 								
